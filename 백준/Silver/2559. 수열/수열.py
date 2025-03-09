ec57ec6a83189ae65384f63b6c3445593e7ed45c
hyeondata <54231475+hyeondata@users.noreply.github.com>
2025-03-09 11:00:06 +0900

    [Silver III] Title: 수열, Time: 80 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,23 +1,13 @@
 
-
-N , K  = list(map(int,input().split()))
+N, K = map(int, input().split())
 temp = list(map(int, input().split()))
 
-psum = [0] * len(temp)
-psum[0] = temp[0]
 
-for i in range(1,N):
-    psum[i] = psum[i-1] + temp[i]
+window_sum = sum(temp[:K])
+max_temp = window_sum
 
+for i in range(K, N):
+    window_sum += temp[i] - temp[i-K]
+    max_temp = max(max_temp, window_sum)
 
-max_temp = -float('inf')
-for i in range(0, N - K+1):
-    if i == 0:
-        max_temp1 = psum[i+K-1]
-    else:
-        max_temp1 = psum[i+K-1] - psum[i-1]
-    if max_temp1 > max_temp:
-        max_temp = max_temp1
 print(max_temp)
-
-
